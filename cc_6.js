84ef8f93a47428200f1d59fd8d7dc099e6a529c1
Asce260901 <ajm55@usf.edu>
2026-02-25 15:53:47 -0500

    Const employyes and managers created

--- cc_6.js
+++ cc_6.js
@@ -7,6 +7,7 @@ class Employee {
         return `${this.name} works in ${this.department}`;
     }
 };
+
 class Manager extends Employee{
     constructor(name,department,teamSize){
         super(name,department);
@@ -16,4 +17,30 @@ class Manager extends Employee{
         return `${this.name} manages a team of ${this.teamSize} people in ${this.department}`;
     }
 };
+
+const employees = [
+    new Employee("John Birck","IT"),
+    new Employee("Kim Young", "HR"),
+    new Employee("Monique Santer", "Finance"),
+    new Employee("Tim Short", "HR"),
+    new Employee("Logan Kurk", "IT"),
+    new Employee("Harvew Specter", "Finance"),
+    new Employee("Louis Lit", "IT"),
+    new Employee("Donna Ferman", "Finance"),
+    new Employee("Wanda Loner","IT")
+];
+for (const emp of employees) {
+    console.log(emp.describe());
+};
+
+const managers = [
+    new Manager("Carl Nickelson","IT",4),
+    new Manager("Lara Croft","HR",2),
+    new Manager("Frank Looney","Finance", 3)
+];
+for (const mgr of managers) {
+    console.log(mgr.describe());
+};
+
+
 
