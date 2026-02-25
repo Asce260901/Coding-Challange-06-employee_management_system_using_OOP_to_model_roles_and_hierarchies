189d5e629cc908e2d55af21811155ed6d5e8c09c
Asce260901 <ajm55@usf.edu>
2026-02-25 15:46:46 -0500

    Initial commit all files created and two clases created

---
+++ cc_6.js
@@ -0,0 +1,19 @@
+class Employee {
+    constructor(name,department) {
+        this.name = name;
+        this.department = department;
+        }
+    describe(){
+        return `${this.name} works in ${this.department}`;
+    }
+};
+class Manager extends Employee{
+    constructor(name,department,teamSize){
+        super(name,department);
+        this.teamSize = teamSize;
+    }
+    describe(){
+        return `${this.name} manages a team of ${this.teamSize} people in ${this.department}`;
+    }
+};
+
